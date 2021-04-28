3b7e709164fa431b45cf03259b1e65c389833ab5
RAIMUNDO <rvorellana@uc.cl>
2021-04-28 14:56:21 -0400

    hola

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'artists/:eartist', to: 'artist#show', as: 'artist_show'
   post 'artists', to: 'artists#create', as: 'artist_create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'artists#index'
+  root to: 'artists#index
 end
